cca0516ebebf4192a8cefa604ce801887cf29ccb
sachin huple <sachinhuple09@yahoo.com>
2016-11-23 15:46:33 +0530

    modified templates to disoplay posts from database

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ Created on 22-Nov-2016
 @author: sachin
 '''
 from django.conf.urls import url
-from blog import views
+from . import views
 urlpatterns=[
              url(r'^$', views.post_list, name='post_list'),
              ]
