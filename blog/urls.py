0ccee0218dcd4497bbdaf27ef964ae5de8b9501f
sachin huple <sachinhuple09@yahoo.com>
2016-11-26 17:25:26 +0530

    Added view and template for detailed post

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ Created on 22-Nov-2016
 @author: sachin
 '''
 from django.conf.urls import url
-from . import views
+from blog import views
 urlpatterns=[
              url(r'^$', views.post_list, name='post_list'),
+             url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
              ]
